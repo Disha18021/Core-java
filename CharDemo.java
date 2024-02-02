10ac324bae2ab43a4fda1fca3c589c381363c8b3
Disha18021 <153249930+Disha18021@users.noreply.github.com>
2024-02-02 20:30:21 +0530

    Add files via upload

---
+++ CharDemo.java
@@ -0,0 +1,10 @@
+class CharDemo 
+{ 
+   public static void main(String args[]) 
+    { 
+	char chl, ch2;
+	chl ch2 = 88; 
+	System.out.print("chl and ch2: "); 
+	System.out.println(chl + + ch2);
+	}
+}
