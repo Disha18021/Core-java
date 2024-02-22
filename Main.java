bbeda57faea003e0bb4f9da2d37a54a212a8c667
Disha18021 <153249930+Disha18021@users.noreply.github.com>
2024-02-22 22:13:39 +0530

    Add files via upload

--- Main.java
+++ Main.java
@@ -1,39 +1,29 @@
-// Class definition
-class MyClass {
-    private int num;
-
-    // Default constructor
-    public MyClass() {
-        this.num = 0;
-    }
-
-    // Overloaded constructor
-    public MyClass(int num) {
-        this.num = num;
-    }
-
-    // Copy constructor
-    public MyClass(MyClass other) {
-        this.num = other.num;
-    }
-
-    // Getter method
-    public int getNum() {
-        return num;
-    }
+class Room
+{
+  float length;
+  float breadth;
+  Room(float x, float y)
+  {
+    length = x;
+    breadth = y;
+  }
+  Room(float x)
+  {
+    length = breadth = x;
+  }
+  float area()
+  {
+    return (length * breadth);
+  }
 }
 
-// Main class
-public class Main {
-    public static void main(String[] args) {
-        // Creating objects using different constructors
-        MyClass obj1 = new MyClass(); // Default constructor
-        MyClass obj2 = new MyClass(10); // Overloaded constructor
-        MyClass obj3 = new MyClass(obj2); // Copy constructor
-
-        // Displaying the values
-        System.out.println("Object 1: " + obj1.getNum()); // Output: 0
-        System.out.println("Object 2: " + obj2.getNum()); // Output: 10
-        System.out.println("Object 3: " + obj3.getNum()); // Output: 10
-    }
+public class Main 
+{
+  public static void main(String args[])
+  {
+    Room rect = new Room(10,5);
+    System.out.println(rect.area());
+    Room square = new Room(10);
+    System.out.println(square.area());
+  }
 }
