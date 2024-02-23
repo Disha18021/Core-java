407b623d12ccab309caf4e830259a9fd86cd5b31
Disha18021 <153249930+Disha18021@users.noreply.github.com>
2024-02-23 20:47:59 +0530

    Add files via upload

---
+++ OpBitEquals2.java
@@ -0,0 +1,27 @@
+class OpBitEquals2 {
+
+public static void main(String args[]) {
+
+int a = 1;
+
+int b = 2;
+
+int c = 3;
+
+a |= 4;
+
+b >>= 1;
+
+C <<= 1;
+
+a ^= c;
+
+System.out.println("a = " + a);
+
+System.out.println("b = " + b);
+
+System.out.println("c = " + c);
+
+}
+
+}
